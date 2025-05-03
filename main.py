b2cc201ec0e0408b741931aec4dd37bc26487697
JKSANJAY27 <j.k.sanjay2006@gmail.com>
2025-05-03 11:42:13 +0530

    Ball Acquisition

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from utils import read_video, save_video
 from trackers import PlayerTracker, BallTracker
-from drawers import (PlayerTracksDrawer, BallTracksDrawer)
+from drawers import (PlayerTracksDrawer, BallTracksDrawer, TeamBallControlDrawer)
 from team_assigner import TeamAssigner
+from ball_acquisition import BallAcquisitionDetector
 
 def main():
     video_frames = read_video("input_videos/video_1.mp4")
@@ -17,11 +18,16 @@ def main():
     team_assigner = TeamAssigner()
     player_assignment = team_assigner.get_player_teams_across_frames(video_frames, player_tracks, read_from_stub=True, stub_path="stubs/player_assignment_stub.pkl")
 
+    ball_acquisition_detector = BallAcquisitionDetector()
+    ball_acquisition = ball_acquisition_detector.detect_ball_possession(player_tracks, ball_tracks)
+
     player_tracks_drawer = PlayerTracksDrawer()
     ball_tracks_drawer = BallTracksDrawer()
+    team_ball_control_drawer = TeamBallControlDrawer()
 
-    output_video_frames = player_tracks_drawer.draw(video_frames, player_tracks, player_assignment)
+    output_video_frames = player_tracks_drawer.draw(video_frames, player_tracks, player_assignment, ball_acquisition)
     output_video_frames = ball_tracks_drawer.draw(output_video_frames, ball_tracks)
+    output_video_frames = team_ball_control_drawer.draw(output_video_frames, player_assignment, ball_acquisition)
 
     save_video(output_video_frames, "output_videos/output_video.avi")
     
