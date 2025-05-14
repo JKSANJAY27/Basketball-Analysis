17115d7999ecc0f89d0c3c6252125f8e8acd3fef
JKSANJAY27 <j.k.sanjay2006@gmail.com>
2025-05-14 12:17:41 +0530

    Court Keypoints Detection

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 from utils import read_video, save_video
 from trackers import PlayerTracker, BallTracker
-from drawers import (PlayerTracksDrawer, BallTracksDrawer, TeamBallControlDrawer, PassInterceptionDrawer)
+from drawers import (PlayerTracksDrawer, BallTracksDrawer, TeamBallControlDrawer, PassInterceptionDrawer, CourtKeypointDrawer)
 from team_assigner import TeamAssigner
 from ball_acquisition import BallAcquisitionDetector
 from pass_and_interception_detector import PassAndInterceptionDetector
+from court_keypoint_detector import CourtKeypointDetector
 
 def main():
     video_frames = read_video("input_videos/video_2.mp4")
 
     player_tracker = PlayerTracker("models/player_detector.pt")
     ball_tracker = BallTracker("models/ball_detector_model.pt")
+    court_keypoint_detector = CourtKeypointDetector("models/court_keypoint_detector.pt")
 
     player_tracks = player_tracker.get_object_tracks(video_frames, read_from_stub=True, stub_path="stubs/player_track_stubs.pkl")
     ball_tracks = ball_tracker.get_object_tracks(video_frames, read_from_stub=True, stub_path="stubs/ball_track_stubs.pkl")
+    court_keypoints = court_keypoint_detector.get_court_keypoints(video_frames, read_from_stub=True, stub_path="stubs/court_key_points_stubs.pkl")
+    
     ball_tracks = ball_tracker.remove_wrong_detections(ball_tracks)
     ball_tracks = ball_tracker.interpolate_ball_positions(ball_tracks)
 
@@ -30,11 +34,13 @@ def main():
     ball_tracks_drawer = BallTracksDrawer()
     team_ball_control_drawer = TeamBallControlDrawer()
     pass_interception_drawer = PassInterceptionDrawer()
+    court_keypoint_drawer = CourtKeypointDrawer()
 
     output_video_frames = player_tracks_drawer.draw(video_frames, player_tracks, player_assignment, ball_acquisition)
     output_video_frames = ball_tracks_drawer.draw(output_video_frames, ball_tracks)
     output_video_frames = team_ball_control_drawer.draw(output_video_frames, player_assignment, ball_acquisition)
     output_video_frames = pass_interception_drawer.draw(output_video_frames, passes, interceptions)
+    output_video_frames = court_keypoint_drawer.draw(output_video_frames, court_keypoints)
 
     save_video(output_video_frames, "output_videos/output_video.avi")
     
