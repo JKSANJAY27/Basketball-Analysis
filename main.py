0ee819b19c53bd51cdb1ffcc8f688f4e197d16e6
JKSANJAY27 <j.k.sanjay2006@gmail.com>
2025-05-14 13:26:39 +0530

    Perspective Transformation

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from utils import read_video, save_video
 from trackers import PlayerTracker, BallTracker
-from drawers import (PlayerTracksDrawer, BallTracksDrawer, TeamBallControlDrawer, PassInterceptionDrawer, CourtKeypointDrawer)
+from drawers import (PlayerTracksDrawer, BallTracksDrawer, TeamBallControlDrawer, PassInterceptionDrawer, CourtKeypointDrawer, TacticalViewDrawer)
 from team_assigner import TeamAssigner
 from ball_acquisition import BallAcquisitionDetector
 from pass_and_interception_detector import PassAndInterceptionDetector
 from court_keypoint_detector import CourtKeypointDetector
+from tactical_view_converter import TacticalViewConverter
 
 def main():
     video_frames = read_video("input_videos/video_2.mp4")
@@ -30,17 +31,23 @@ def main():
     passes = pass_and_interception_detector.detect_passes(ball_acquisition, player_assignment)
     interceptions = pass_and_interception_detector.detect_interceptions(ball_acquisition, player_assignment)
 
+    tactical_view_converter = TacticalViewConverter(court_image_path = "./images/basketball_court.png")
+    tactical_view_converter.validate_keypoints(court_keypoints)
+    tactical_player_positions = tactical_view_converter.transform_players_to_tactical_view(court_keypoints, player_tracks)
+
     player_tracks_drawer = PlayerTracksDrawer()
     ball_tracks_drawer = BallTracksDrawer()
     team_ball_control_drawer = TeamBallControlDrawer()
     pass_interception_drawer = PassInterceptionDrawer()
     court_keypoint_drawer = CourtKeypointDrawer()
+    tactical_view_drawer = TacticalViewDrawer()
 
     output_video_frames = player_tracks_drawer.draw(video_frames, player_tracks, player_assignment, ball_acquisition)
     output_video_frames = ball_tracks_drawer.draw(output_video_frames, ball_tracks)
     output_video_frames = team_ball_control_drawer.draw(output_video_frames, player_assignment, ball_acquisition)
     output_video_frames = pass_interception_drawer.draw(output_video_frames, passes, interceptions)
     output_video_frames = court_keypoint_drawer.draw(output_video_frames, court_keypoints)
+    output_video_frames = tactical_view_drawer.draw(output_video_frames, tactical_view_converter.court_image_path, tactical_view_converter.width, tactical_view_converter.height, tactical_view_converter.key_points, tactical_player_positions, player_assignment, ball_acquisition)
 
     save_video(output_video_frames, "output_videos/output_video.avi")
     
--- utils/bbox_utils.py
+++ utils/bbox_utils.py
@@ -11,3 +11,7 @@ def measure_distance(p1,p2):
 def get_center_of_bbox(bbox):
     x1, y1, x2, y2 = bbox
     return int((x1 + x2) / 2), int((y1 + y2) / 2)
+
+def get_foot_position(bbox):
+    x1, y1, x2, y2 = bbox
+    return int((x1 + x2) / 2), int(y2)
